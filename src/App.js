9b2aa37c0150e55d9dd75d7626ab33af525768c1
The Black Cat <70791006+TheBlckCat@users.noreply.github.com>
2022-08-08 18:32:34 -0400

    Add time for work and time for take a break

--- src/App.js
+++ src/App.js
@@ -11,49 +11,73 @@ import { Container } from '@mui/system';
 
 const theme = createTheme({
   palette: {
-      primary:{
-        /*main: "#8689AC"*/
-        main: "#283347"
-      },
-      secondary: {
-        main: "#8689AC"
-      },
-      background: {
-        /*default:'#2F3148'*/
-        default: "#586c91"
-      },
-      text: {
-        /*primary: '#EDE8DF',*/
-        primary: "#f4f7fc"
-      },
-      buttom: {
-        borderColor: "#f4f7fc"
-      }
-
+    primary: {
+      main: "#8689AC"
+      /*main: "#283347"*/
+    },
+    secondary: {
+      main: "#8689AC"
+    },
+    background: {
+      default: '#2F3148'
+      /*default: "#586c91"*/
+    },
+    text: {
+      primary: '#EDE8DF',
+      /*primary: "#f4f7fc"*/
+    },
   },
 });
 
 export const App = () => {
-  const [initialTime, setInitialTime] = useState(0);
-  const [time, setTime] = useState(0);
+  const [worktime, setWorktime] = useState(10);
+  const [breaktime, setBreaktime] = useState(5);
+  const [mode, setMode] = useState("work")
+  const [isActive, setIsActive] = useState(false)
+  const [time, setTime] = useState(10);
+  const [progress, setProgress] = useState(100);
 
+  const start = () => {
+    setIsActive(true)
+  }
 
-  const start = (e) => {
-    setInitialTime(2 * 60)
-    setTime(2 * 60)
+  const pause = () => {
+    setIsActive(false)
+  }
+
+  const reset = () => {
+    setWorktime(0)
+    setTime(0)
+    setIsActive(false)
+    setProgress(100)
+  }
+
+  const selectState = () => {
+    isActive === false ? start() : pause()
+  }
+
+  const changeMode = (mode, worktime, breaktime) => {
+    const nextMode = mode === "work" ? "break" : "work"
+    const nextTime = mode === "work" ? breaktime : worktime
+
+    setMode(nextMode)
+    setTime(nextTime)
   }
 
   useEffect(() => {
-
+    setProgress((time * 100) / (mode === "work" ? worktime : breaktime ) )
     const interval = setInterval(() => {
-      clearInterval(interval);
-      if (time !== 0) {
-        setTime(time - 1)
+      if (isActive === false) {
+        return;
       }
+      if (time === 0) {
+        return changeMode(mode, worktime, breaktime)
+      }
+      setTime(time - 1)
     }, 1000)
-  }, [initialTime, time])
 
-  const progress = (time / (initialTime / 100));
+    return () => clearInterval(interval);
+  }, [mode, isActive, time, worktime, breaktime])
 
   const timerSeconds = (time) => {
     const minutes = Math.floor(time / 60);
@@ -77,17 +101,17 @@ export const App = () => {
       <Container sx={{ display: 'flex', justifyContent: "center", height: "100vh", alignItems: "center" }}>
         <Box className='timer' sx={{ display: 'flex', flexDirection: "column", alignItems: "center" }}>
           <Box sx={{ position: "relative", textAlign: "center" }}>
-            <Typography variant="h1" sx={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", color:"#ffffff"}}>{timerSeconds(time)}</Typography>
+            <Typography variant="h1" sx={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>{timerSeconds(time)}</Typography>
             <CircularProgress variant="determinate" sx={{ color: "#283347" }} size={400} value={100} />
-            <CircularProgress variant="determinate" sx={{ position: "absolute", left: 0, color: "#dfe7f2" }} size={400} value={progress} />
+            <CircularProgress variant="determinate" sx={{ position: "absolute", left: 0, color: mode === "work" ? "#f54768" : "#6bbd99" }} size={400} value={progress} />
           </Box>
           <Grid container className='settings' sx={{ padding: "1.25rem" }} justifyContent="center" spacing={1}>
             <Grid item>
-              <Button variant="outlined" onClick={e => { start(e) }}>Start</Button>
+              <Button variant="outlined" onClick={() => { selectState() }}>{isActive === false ? "Start" : "Pause"}</Button>
             </Grid>
 
             <Grid item>
-              <Button variant="outlined" onClick={e => { start(e) }}>Settings</Button>
+              <Button variant="outlined" disabled={isActive === false} onClick={() => { reset() }}>Reset</Button>
             </Grid>
           </Grid>
         </Box>
